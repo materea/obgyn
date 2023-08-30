4e267b69f68f2111f67cf78665abeb97defece0b
veet-co <115666266+materia-ob-gyn@users.noreply.github.com>
2023-08-30 17:46:06 +0100

    new home page, outline, modal images, etc

--- main.js
+++ main.js
@@ -5,3 +5,42 @@ function scrollUp(){
     if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+
+// search bar
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const topics = document.querySelectorAll(".topics-container li");
+  
+    searchInput.addEventListener("input", function () {
+      const searchTerm = searchInput.value.trim().toLowerCase();
+  
+      topics.forEach(topic => {
+        const topicText = topic.textContent.toLowerCase();
+        if (topicText.includes(searchTerm)) {
+          topic.style.display = "block";
+        } else {
+          topic.style.display = "none";
+        }
+      });
+    });
+  });
+
+
+// modal image
+  function openModal(imageElement) {
+    var modal = document.getElementById("myModal");
+    var modalImage = document.getElementById("modalImage");
+    var caption = document.getElementById("caption");
+
+  
+    modal.style.display = "block";
+    modalImage.src = imageElement.src;
+    caption.innerHTML = imageElement.nextElementSibling.innerHTML;
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+  
