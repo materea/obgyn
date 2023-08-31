16f2ad52e69c04c9b129f180ccbebc38238072fa
veet-co <115666266+materia-ob-gyn@users.noreply.github.com>
2023-08-31 07:37:07 +0100

    fixing modal images, search js

--- main.js
+++ main.js
@@ -7,40 +7,6 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp)
 
 
-// search bar
-document.addEventListener("DOMContentLoaded", function () {
-    const searchInput = document.getElementById("searchInput");
-    const topics = document.querySelectorAll(".topics-container li");
-  
-    searchInput.addEventListener("input", function () {
-      const searchTerm = searchInput.value.trim().toLowerCase();
-  
-      topics.forEach(topic => {
-        const topicText = topic.textContent.toLowerCase();
-        if (topicText.includes(searchTerm)) {
-          topic.style.display = "block";
-        } else {
-          topic.style.display = "none";
-        }
-      });
-    });
-  });
 
 
-// modal image
-  function openModal(imageElement) {
-    var modal = document.getElementById("myModal");
-    var modalImage = document.getElementById("modalImage");
-    var caption = document.getElementById("caption");
 
-  
-    modal.style.display = "block";
-    modalImage.src = imageElement.src;
-    caption.innerHTML = imageElement.nextElementSibling.innerHTML;
-  }
-  
-  function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-  }
-  
