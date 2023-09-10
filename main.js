64505a605330c219aad2ede79b31607f4fbd3983
veet-co <115666266+materia-ob-gyn@users.noreply.github.com>
2023-09-10 22:36:10 +0100

    footer, etc, ped

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp)
 
 
+// JavaScript function to show the pop-up dive
+function showPopup() {
+var popup = document.getElementById("popup");
+popup.style.display = "block";
+}
 
+// JavaScript function to close the pop-up dive
+function closePopup() {
+var popup = document.getElementById("popup");
+popup.style.display = "none";
+}
+
+// Add a click event listener to the text element
+var trigger = document.getElementById("popup-trigger");
+trigger.addEventListener("click", showPopup);
 
 
